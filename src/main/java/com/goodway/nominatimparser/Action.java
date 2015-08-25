2f982e5dcfe176a350839f46472e0f875b077e16
Antoine Sauray <sauray.a@outlook.com>
2015-08-25 17:57:48 +0200

    Templated action

--- src/main/java/com/goodway/nominatimparser/Action.java
+++ src/main/java/com/goodway/nominatimparser/Action.java
@@ -3,6 +3,6 @@ package com.goodway.nominatimparser;
 /**
  * Created by antoine on 8/19/15.
  */
-public interface Action {
-    public void action(Place e);
+public interface Action<T> {
+    public void action(T e);
 }
